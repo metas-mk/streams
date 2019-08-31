1930e5d63d31e94df1aa366233145a1b77fbfb7a
metas-mk <mark.krake@metasfresh.com>
2019-08-31 13:09:54 +0200

    Adding logic to google Auth Component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '293501909631-886gvf9254f4ltm8idakop16ntstvpae.apps.googleusercontent.com',
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '293501909631-886gvf9254f4ltm8idakop16ntstvpae.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          // update component level state shall rerender the component
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if I am signed in.</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>{this.renderAuthButton()}</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
