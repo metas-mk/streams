e6f60a1715eebddbcf2cb71b2bb6b08242a0f666
metas-mk <mark.krake@metasfresh.com>
2019-09-01 14:33:44 +0200

    Adds Redux. Initial creation for action creators and reducers

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -24,11 +24,11 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -39,14 +39,14 @@ class GoogleAuth extends React.Component {
       return null; // not showing anything when status is not clear
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui green google button">
+        <button onClick={this.onSignInClick} className="ui green google button">
           <i className="google icon" />
           Sign In with Google
         </button>
