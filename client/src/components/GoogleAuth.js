db07eaec7d08975e29e031741ebd32f4dc6f70f5
metas-mk <mark.krake@metasfresh.com>
2019-09-01 11:46:17 +0200

    Adds onClick event Handlers

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -24,6 +24,14 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     //console.log(this.state);
 
@@ -31,14 +39,14 @@ class GoogleAuth extends React.Component {
       return null; // not showing anything when status is not clear
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignOut} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui green google button">
+        <button onClick={this.onSignIn} className="ui green google button">
           <i className="google icon" />
           Sign In with Google
         </button>
