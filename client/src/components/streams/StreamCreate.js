e737f730572d0c69ef23c5cb4c15753d5ffb603f
metas-mk <mark.krake@metasfresh.com>
2019-09-04 18:07:49 +0200

    Adds Field component and wired with input

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+
+    return <input />;
+  }
+
   render() {
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
