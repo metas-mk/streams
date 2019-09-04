fd190d5ac9dd8fb67ddc132576b3d419e56872ca
metas-mk <mark.krake@metasfresh.com>
2019-09-04 18:28:33 +0200

    Destructuring the input props

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
-    console.log(formProps);
-
-    return <input />;
+  renderInput({ input }) {
+    return <input {...input} />;
   }
 
   render() {
