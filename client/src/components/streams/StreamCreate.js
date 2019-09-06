9ff889aa245e2aac473e660fd7f9a5ba6efb430a
metas-mk <mark.krake@metasfresh.com>
2019-09-06 17:24:26 +0200

    Adds logic for error display and styling

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -13,9 +13,9 @@ class StreamCreate extends React.Component {
   };
 
   renderInput = ({ input, label, meta }) => {
-    console.log(meta);
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
         <div>{this.renderError(meta)}</div>
@@ -31,7 +31,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
