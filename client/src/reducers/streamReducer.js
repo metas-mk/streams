dfb97d391f3efcea7b4e6cd833c905dbede07405
metas-mk <mark.krake@metasfresh.com>
2019-09-08 16:00:17 +0200

    Adds streamReducer

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import {
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS
+} from '../actions/types';
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case CREATE_STREAM:
+    case EDIT_STREAM:
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
